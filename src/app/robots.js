4cd326a847875e8990bc66d841848e9ee46434d5
Tran Thi Luyen <tranthiluyen@MacBook-Pro-cua-Tran.local>
2026-03-06 06:51:06 +0700

    fix sitemap

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://mrchinesechannel.com';
+const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://mrchinese.viendong.online/';
 
 export default function robots() {
   return {
